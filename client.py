ab20e70df3739fa6f3482791398e23a03c6aba4e
Pavel Studeník <pavel.studenik@kiwi.com>
2020-11-03 21:43:46 +0100

    Read data from camera

--- client.py
+++ client.py
@@ -1,48 +1,12 @@
-import base64
-import sys
-from time import sleep, time
-
 import requests
 
-X_TOKEN = "token"
+url = "http://localhost:8080/device"
 
+X_TOKEN = "token_xxx_xxx"
 
-def load_image(filename):
-    with open(filename, "rb") as fr:
-        return fr.read()
+headers = {
+            "X-Token": X_TOKEN,
+        }
 
-
-def check():
-    url = "http://localhost:8080/config"
-    headers = {
-        "X-Token": X_TOKEN,
-    }
-    response = requests.get(url, headers=headers)
-    return response.json()
-
-
-def upload(filename):
-    url = "http://localhost:8080/push"
-    binary = load_image(filename)
-    headers = {
-        "X-Token": X_TOKEN,
-    }
-    data = base64.b64encode(binary).decode('ascii')
-    response = requests.post(url, json={"type": "base64", "data": data, "timestamp": int(time())}, headers=headers)
-    print(response.json())
-
-
-def main(filename=None):
-    while True:
-        try:
-            config = check()["config"]
-            print(config, "...")
-            sleep(config["refresh"])
-        except KeyboardInterrupt:
-            break
-    if filename:
-        upload(filename)
-
-
-if __name__ == "__main__":
-   main(*sys.argv[1:])
+r = requests.get(url, headers=headers)
+print(r.text)
